80c085f844eb1ee3fd887adf9550338608995619
victoria verosova <victoria_vvv@mail.ru>
2017-03-28 23:08:43 +0300

    with month change

--- datepicker_homework/insertDate.js
+++ datepicker_homework/insertDate.js
@@ -8,11 +8,27 @@
         "07", "08", "09", "10", "11", "12"
     ];
     var today = new Date();
-    this.insertDate = function insert(e) {
-        var selectedDate = e.innerText;
-        document.querySelector('.datepicker--input').value = monthValues[today.getMonth()] + "/" + selectedDate + "/" + today.getFullYear();
-        document.querySelector('.datepicker--calendar').style.display = "none";
-        if (lastSelectedDate !== "") {
+    var currentDate = today;
+
+    var calendar = document.querySelector(".calendar--numbers");
+    var dateInputField = document.querySelector(".datepicker--input");
+    var nextMonthBtn = document.querySelector(".calendar--next");
+    var previousMonthBtn = document.querySelector(".calendar--previous");
+
+    calendar.addEventListener("click", insertDate);
+    dateInputField.addEventListener("click", showCalendar);
+    nextMonthBtn.addEventListener("click", showNextMonth);
+    previousMonthBtn.addEventListener("click", showPreviousMonth);
+
+
+    function insertDate(e) {
+        var selectedDate = e.target.innerText;
+        if (selectedDate != "") {
+            document.querySelector('.datepicker--input').value = monthValues[currentDate.getMonth()] +
+                "/" + selectedDate + "/" + currentDate.getFullYear();
+            document.querySelector('.datepicker--calendar').style.display = "none";
+        }
+        if (lastSelectedDate != "") {
             document.querySelectorAll(".calendar--num")[lastSelectedDate - 1]
                 .setAttribute("style", "background-color:white");
         }
@@ -23,40 +39,73 @@
         lastSelectedDate = selectedDate;
     }
 
-    this.showCalendar = function date() {
+    function showCalendar() {
+
         if (document.querySelector('.datepicker--calendar').style.display == "none") {
 
             document.querySelector('.datepicker--calendar').style.display = "block";
 
+            if (!firstOpen) {
+                removeOldCalendar();
+            }
+
+            document.querySelector('.calendar--month-current').innerText = monthNames[currentDate.getMonth()] + " " + currentDate.getFullYear();
+
+            var day = today.getDay();
+            var daysTotal = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate();
+            var dayFirst = new Date(currentDate.getFullYear(), (currentDate.getMonth()), 1).getDay();
+
+
+            var list = document.querySelector(".calendar--numbers");
+            for (var i = 0; i < dayFirst; i++) {
+                list.insertAdjacentHTML('beforeEnd', '<div class="calendar--num__notDate" ></div>');
+            }
+            for (var i = 0; i < daysTotal; i++) {
+                if (i < daysTotal) {
+                    list.insertAdjacentHTML('beforeEnd',
+                        '<div class="calendar--num"></div>');
+                }
+                document.querySelectorAll(".calendar--num")[i].innerText = i + 1;
+                if ((i + 1 == today.getDate()) && (today.getMonth() == currentDate.getMonth()) && (today.getFullYear() == currentDate.getFullYear())) {
+                    document.querySelectorAll(".calendar--num")[i].setAttribute("style", "background-color:yellow");
+                }
+            }
             if (lastSelectedDate !== "") {
                 document.querySelectorAll(".calendar--num")[lastSelectedDate - 1].setAttribute("style", "background-color:blue");
             }
 
+            firstOpen = false;
 
-            if (firstOpen) {
-                firstOpen = false;
-                document.querySelector('.calendar--month').innerText = monthNames[today.getMonth()] + " " + today.getFullYear();
+        }
+    }
 
-                var day = today.getDay();
-                var daysTotal = new Date(today.getFullYear(), today.getMonth() + 1, 0).getDate();
-                var dayFirst = new Date(today.getFullYear(), (today.getMonth()), 1).getDay();
+    function showNextMonth() {
+        document.querySelector('.datepicker--calendar').style.display = "none";
+        currentDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 1);
+        firstOpen = false;
+        showCalendar();
+        correctInputValueOnMonthChange();
+    }
 
-                var list = document.querySelector(".calendar--numbers");
-                for (var i = 0; i < dayFirst; i++) {
-                    list.insertAdjacentHTML('beforeEnd', '<div class="calendar--num__notDate" ></div>');
-                }
+    function showPreviousMonth() {
+        document.querySelector('.datepicker--calendar').style.display = "none";
+        currentDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0);
+        firstOpen = false;
+        showCalendar();
+        correctInputValueOnMonthChange();
+    }
 
-                for (var i = 0; i < daysTotal; i++) {
-                    if (i < daysTotal) {
-                        list.insertAdjacentHTML('beforeEnd',
-                            '<div class="calendar--num" onclick="insertDate(this)"></div>');
-                    }
-                    document.querySelectorAll(".calendar--num")[i].innerText = i + 1;
-                    if (i + 1 == today.getDate()) {
-                        document.querySelectorAll(".calendar--num")[i].setAttribute("style", "background-color:yellow");
-                    }
-                }
-            }
+    function correctInputValueOnMonthChange() {
+        if (lastSelectedDate != "") {
+            document.querySelector('.datepicker--input').value = monthValues[currentDate.getMonth()] +
+                "/" + lastSelectedDate + "/" + currentDate.getFullYear();
+        }
+    }
+
+    function removeOldCalendar() {
+        var days = document.querySelectorAll(".calendar--num, .calendar--num__notDate");
+        for (var i = 0; i < days.length; i++) {
+            (document.querySelector(".calendar--numbers")).removeChild(days[i]);
         }
     }
 
